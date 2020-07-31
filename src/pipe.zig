55f62e2fa77824eebb1f942b70849eabd5f2fce9
SuperAuguste <auguste.rame@gmail.com>
2020-07-31 12:27:20 -0400

    Finish fixing tests!

--- src/pipe.zig
+++ src/pipe.zig
@@ -58,55 +58,55 @@ pub fn pipe(comptime args: anytype) fn (InputType(getFields(args))) ReturnType(g
     }.call;
 }
 
-fn addOne(num: u8) u8 {
-    return num + 1;
-}
-
-fn addOneError(num: u8) !u8 {
-    if (num == 69) return error.Nice;
-    return num * 2;
-}
-
-fn addX(comptime x: usize) fn (usize) usize {
-    return struct {
-        fn add(in: usize) usize {
-            return in + x;
-        }
-    }.add;
-}
-
-fn subX(comptime x: usize) fn (usize) usize {
-    return struct {
-        fn add(in: usize) usize {
-            return in - x;
-        }
-    }.add;
-}
-
-fn mulX(comptime x: usize) fn (usize) usize {
-    return struct {
-        fn add(in: usize) usize {
-            return in * x;
-        }
-    }.add;
-}
-
 test "Pipe" {
+    const TestFunctions = struct {
+        fn addOne(num: u8) u8 {
+            return num + 1;
+        }
+
+        fn addOneError(num: u8) !u8 {
+            if (num == 69) return error.Nice;
+            return num * 2;
+        }
+
+        fn addX(comptime x: usize) fn (usize) usize {
+            return struct {
+                fn add(in: usize) usize {
+                    return in + x;
+                }
+            }.add;
+        }
+
+        fn subX(comptime x: usize) fn (usize) usize {
+            return struct {
+                fn add(in: usize) usize {
+                    return in - x;
+                }
+            }.add;
+        }
+
+        fn mulX(comptime x: usize) fn (usize) usize {
+            return struct {
+                fn add(in: usize) usize {
+                    return in * x;
+                }
+            }.add;
+        }
+    };
 
     const add_pipe = pipe(.{
-        addOne,
-        addOneError
+        TestFunctions.addOne,
+        TestFunctions.addOneError
     });
 
     std.testing.expectEqual(@as(u8, 22), try add_pipe(10));
     std.testing.expectError(error.Nice, add_pipe(68));
 
     const addX_pipe = pipe(.{
-        comptime addX(5),
-        comptime mulX(10),
-        comptime subX(10)
+        comptime TestFunctions.addX(5),
+        comptime TestFunctions.mulX(10),
+        comptime TestFunctions.subX(10)
     });
 
     std.testing.expectEqual(@as(usize, 190), addX_pipe(15));
-
 }
