bb486747e589f5fc2ccbd4302b15bfce381af600
SuperAuguste <auguste.rame@gmail.com>
2021-01-02 18:42:26 -0500

    Add map that doesn't require alloc

--- src/map.zig
+++ src/map.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
 
+pub fn map(comptime func: anytype) fn ([]@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?, []@typeInfo(@TypeOf(func)).Fn.return_type.?) anyerror!void {
+    return struct {
+        fn call(arr: []@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?, out: []@typeInfo(@TypeOf(func)).Fn.return_type.?) anyerror!void {
+            for (arr) |cur, idx|
+                out[idx] = func(cur);
+        }
+    }.call;
+}
+
 pub fn mapAlloc(comptime allocator: *std.mem.Allocator, comptime func: anytype) fn ([]@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?) anyerror![]@typeInfo(@TypeOf(func)).Fn.return_type.? {
     return struct {
         fn call(arr: []@typeInfo(@TypeOf(func)).Fn.args[0].arg_type.?) anyerror![]@typeInfo(@TypeOf(func)).Fn.return_type.? {
@@ -12,14 +21,27 @@ pub fn mapAlloc(comptime allocator: *std.mem.Allocator, comptime func: anytype) 
 }
 
 test "Map" {
-    const testMap = mapAlloc(std.testing.allocator, struct {
+    const testMap = map(struct {
         fn call(in: u8) u8 {
             return in + 1;
         }
     }.call);
 
     var arr = [_]u8{'a', 'b', 'c'};
-    var out = try testMap(&arr);
+    var out = [_]u8{0} ** 3;
+    try testMap(&arr, &out);
+    std.testing.expectEqualSlices(u8, &[_]u8{'b', 'c', 'd'}, &out);
+}
+
+test "Map w/ Allocations" {
+    const testMapAlloc = mapAlloc(std.testing.allocator, struct {
+        fn call(in: u8) u8 {
+            return in + 1;
+        }
+    }.call);
+
+    var arr = [_]u8{'a', 'b', 'c'};
+    var out = try testMapAlloc(&arr);
     defer std.testing.allocator.free(out);
     std.testing.expectEqualSlices(u8, &[_]u8{'b', 'c', 'd'}, out);
 }
